fb2d58f6dab73cf71f618207b6428b606e71300d
Suman Maiti <126709426+SumanRigel@users.noreply.github.com>
2025-03-20 19:41:42 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+let itemsContainerElement = document.querySelector('.items-container');
+itemsContainerElement.innerHTML=`<div class="item-container">
+    <img class ="item-image" src="images/1.jpg" alt="item image">
+    <div class ="rating">
+        4.5 |1.4k
+    </div>
+    <div class="company-name"> Carlton London</div>
+    <div class="item-name"> Rhodium-Plated CZ Floral Studs</div>
+    <div class="price">
+        <span class="current-price"> Rs 606</span>
+        <span class="original-Price"> Rs 1045</span>
+        <span class="discount"> (42% OFF)</span>
+    </div>
+    <button class="btn-add-bag"> Add to Bag</button>
+    </div>`;
+
+
